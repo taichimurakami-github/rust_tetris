906d0a8197dcdc41b07bfd8644680ee9a7a2bb5a
Taichi Murakami <64308722+taichimurakami-github@users.noreply.github.com>
2022-01-31 19:08:10 +0900

    参考文献を解析中

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,1 @@
-struct Pos {
-    x: i32,
-    y: i32,
-}
-fn main() {
-    let p = Pos { x: 2, y: 1 };
-    let r = rotate(p, 90);
-
-    println!("{},{}", r.x, r.y);
-}
-
-fn rotate(pos: Pos, dir: i32) -> Pos {
-    println!("{}", &dir);
-    return Pos {
-        x: pos.x * 2,
-        y: pos.y * 2,
-    };
-}
+fn main() {}
